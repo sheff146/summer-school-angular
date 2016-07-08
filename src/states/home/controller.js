9c3514064bb565155c9a6a2293681cd4c765ab73
a.pihtovnikov <a.pihtovnikov@xsolla.com>
2016-07-08 19:07:14 +0500

    Добавлены примеры на "Главную" страницу

--- src/states/home/controller.js
+++ src/states/home/controller.js
@@ -13,6 +13,37 @@ angular.module('ngs.states').controller('homeCtrl', function ($scope, StateCtrl)
             
             // Метку помещаем в переменную только для демонстрации тестов
             this.scope.demoText = 'DEMO_DATA';
+
+            // Демонстрация изменения модели (demoModel2) из контроллера
+            this.scope.demoModel2 = 'Тестовые данные';
+
+            // Демонстрация изменения модели (demoModel3) из контроллера. В модель записывается объект
+            this.scope.demoModel3 = {
+                demoText: 'Демонстрационный текст',
+                demoProducts: [
+                    {
+                        name: 'Яблоко',
+                        category: 'Фрукты',
+                        price: 30
+                    },
+                    {
+                        name: 'Груша',
+                        category: 'Фрукты',
+                        price: 41
+                    },
+                    {
+                        name: 'Mac Pro',
+                        category: 'Компы',
+                        price: 50000
+                    },
+                    {
+                        name: 'Кирпич',
+                        category: 'Разное',
+                        price: 24
+                    }
+                ]
+            };
+
         }
     }
 
