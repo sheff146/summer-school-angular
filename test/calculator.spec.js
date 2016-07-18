a5715a323c4f9080b022ef9e8094d8400e0830c6
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2016-07-18 20:53:56 +0500

    Add spy test

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -1,39 +1,46 @@
-describe('Calculator tests', function () {
-    var calculator, mockSettingsService;
-    var rate = 11.5;
+var calculator, mockSettingsService;
+var rate = 11.5;
 
-    beforeEach(function () {
-        module('ngs.services')
+var createMockSettingsService = function () {
+    module('ngs.services');
 
-        mockSettingsService = {
-            get: function (name) {
-                if (name === 'rate') {
-                    return rate;
-                }
+    mockSettingsService = {
+        get: function (name) {
+            if (name === 'rate') {
+                return rate;
             }
-        };
+        }
+    };
+};
 
-        module(function ($provide) {
-            $provide.value('settingsService', mockSettingsService);
-        });
+var createCalculator = function () {
+    module(function ($provide) {
+        $provide.value('settingsService', mockSettingsService);
+    });
 
-        inject(function (calculatorService) {
-            calculator = calculatorService;
-        });
+    inject(function (calculatorService) {
+        calculator = calculatorService;
+    });
+};
+
+function getDataForCalc(price, firstPayment, period) {
+    return {
+        price: price,                   // стоимость квартиры (руб.)
+        firstPayment: firstPayment,     // размер первого платежа (%)
+        period: period                  // срок выплат (лет)
+    };
+}
+
+describe('Calculator tests', function () {
+    beforeEach(function () {
+        createMockSettingsService();
+        createCalculator();
     });
 
     afterEach(function () {
         calculator = null;
         mockSettingsService = null;
     });
-
-    function getDataForCalc(price, firstPayment, period) {
-        return {
-            price: price,                   // стоимость квартиры (руб.)
-            firstPayment: firstPayment,     // размер первого платежа (%)
-            period: period                  // срок выплат (лет)
-        };
-    }
 
     it('should check the rate', function () {
         // A...
@@ -42,5 +49,31 @@ describe('Calculator tests', function () {
         var result = calculator.count(data);
         // A...
         expect(result.rate).toBe(rate);
+    });
+});
+
+describe('Spy tests', function () {
+    beforeEach(function () {
+        createMockSettingsService();
+
+        spyOn(mockSettingsService, 'get');
+
+        createCalculator();
+    });
+
+    afterEach(function () {
+        calculator = null;
+        mockSettingsService = null;
+    });
+
+    it('should call settingsService once', function () {
+        // A...
+        var data = getDataForCalc(3000000, 30, 20);
+        // A...
+        var result = calculator.count(data);
+        // A...
+        expect(mockSettingsService.get).toHaveBeenCalled();
+        expect(mockSettingsService.get).toHaveBeenCalledTimes(1);
+        expect(mockSettingsService.get).toHaveBeenCalledWith('rate');
     });
 });
